09731ac3752ecc4a0309ab6f9136eddd1f773259
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-02-18 01:29:50 +0900

    Create sort-colors.cpp

---
+++ c++/leetcode/sort-colors.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> result;
+        return sort(nums.begin(),nums.end());
+    }
+};
+
+//https://leetcode.com/problems/sort-colors
