6292f4e228df1dc9f88a7efa4402cc81ffe09232
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-03-06 17:24:05 +0900

    Create 1이될때까지.cpp

---
+++ c++/이것이취업을위한코딩테스트다/1이될때까지.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int a, b;
+	cin >> a >> b;
+	int result = 0;
+	while (a != 1) {
+		if (a%b == 0) {
+			a = a / b;
+		}
+		else {
+			a = a - 1;
+		}
+		result++;
+	}
+	cout << result << endl;
+	return 0;
+}
