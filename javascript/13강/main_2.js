8038701b9231b37096fde8131f75529e34ff596b
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-03-14 20:02:14 +0900

    Delete javascript directory

--- javascript/13강/main_2.js
+++
@@ -1,9 +0,0 @@
-const el = document.querySelector('div');
-const btn = document.querySelector('button');
-btn.addEventListener('click', function () {
-    if (el.classList.contains('hide')) {
-        el.classList.remove('hide');
-    } else {
-        el.classList.add('hide');
-    }
-});
