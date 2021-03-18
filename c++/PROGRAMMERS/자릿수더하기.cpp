fb86d92e1a8b250c3b42ca0c6a62fd7b2a57de16
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-03-19 01:06:28 +0900

    Create 자릿수더하기.cpp

---
+++ c++/PROGRAMMERS/자릿수더하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+    while(n){
+        answer=answer+(n%10);
+        n=n/10;
+    }
+    return answer;
+}
