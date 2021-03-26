e2341a3eb78f7f09e4da2190796858a1e19ef14c
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-03-27 01:17:43 +0900

    Create 콜라츠정리.cpp

---
+++ c++/PROGRAMMERS/콜라츠정리.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    while(n!=1){
+        if(n%2==0){
+            n/=2;
+        }
+        else{
+            n=n*3+1;
+        }
+        answer++;
+        if(answer==500){
+            answer=-1;
+            break;
+        }
+    }
+    return answer;
+}
