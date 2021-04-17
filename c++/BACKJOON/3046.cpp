9ed9831548a4625b488ca74497ccad5e70a47cee
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-04-17 19:22:16 +0900

    Create 3046.cpp

---
+++ c++/BACKJOON/3046.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie();
+	int a, b;
+	cin >> a >> b;
+	int x = 2*b - a;
+	cout << x << endl;
+	return 0;
+}
