f53237adb3b4fbd95a0cd2c762620238d3c4480f
김용수 <55073153+dolmac1@users.noreply.github.com>
2021-09-04 18:26:39 +0900

    Create _11866.cpp

---
+++ c++/BACKJOON/_11866.cpp
@@ -0,0 +1,30 @@
+//요세푸스 s4
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie();
+	int n, k;
+	cin >> n >> k;
+	vector<int> arr;
+	for (int i = 0;i < n;i++) {
+		arr.push_back(i + 1);
+	}
+	int erase_val = 0;
+	cout << "<";
+	for (int i = 0;i < arr.size();) {
+		erase_val = (erase_val + k -1) % arr.size();
+		cout << arr[erase_val];
+		arr.erase(arr.begin() + erase_val);
+		if (!arr.empty()) {
+			cout << ", ";
+		}
+	}
+	cout << ">";
+	return 0;
+}
